f3842338dc28e7c26f3f596520fda1989897cb35
qwukker <minor7addfloortom@gmail.com>
2024-02-13 14:41:21 -0600

    Added a function to generate metadata flow in the YouTube script generation process.

--- indy_dev_tools/commands/youtube.py
+++ indy_dev_tools/commands/youtube.py
@@ -2,6 +2,7 @@ import typer
 from indy_dev_tools.commands import thumbnails, titles, script, descriptions
 from indy_dev_tools.models import IdtConfig
 from indy_dev_tools.modules.file_util import get_path_to_files_with_sound
+from indy_dev_tools.modules.generate_metadata_flow import generate_metadata_flow
 from indy_dev_tools.modules.idt_config import load_config
 from indy_dev_tools.modules import (
     create_thumbnail,
@@ -151,27 +152,13 @@ def gen_meta(
 ):
     typer.echo("Generating meta data")
 
-    # Transcribe 120 seconds to create the script
-    create_transcription.create_transcription(
-        path_to_audio_or_video, duration_limit_in_seconds=220, create_json_file=True
+    generate_metadata_flow(
+        path_to_audio_or_video,
+        rough_draft_title,
+        thumbnail_prompt,
+        seo_keywords,
+        count,
     )
-
-    # Generate titles
-    create_title.create_title(
-        count, rough_draft_title, config_file.yt.script_file_path, seo_keywords
-    )
-
-    # Generate descriptions
-    create_description.create_description(
-        count, config_file.yt.script_file_path, seo_keywords
-    )
-
-    # Generate thumbnails
-    create_thumbnail.create_thumbnail(count, thumbnail_prompt)
-
-    # Assuming there's a function for resizing in create_thumbnails
-    for i in range(count):
-        resize_image.resize_image(config_file.yt.make_thumbnail_file_path(i))
 
 
 if __name__ == "__main__":

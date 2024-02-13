9fccd02d1eeb374aca322a22b791990a4c64c28a
qwukker <minor7addfloortom@gmail.com>
2024-02-13 15:31:10 -0600

    Refactor format_references command to format command and update argument descriptions.

--- indy_dev_tools/commands/references.py
+++ indy_dev_tools/commands/references.py
@@ -1,15 +1,21 @@
 import typer
-from indy_dev_tools.modules.create_formatted_references import create_formatted_references
+from indy_dev_tools.modules.create_formatted_references import (
+    create_formatted_references,
+)
 
 app = typer.Typer()
 
+
 @app.command()
-def format_references(
+def format(
     references: str = typer.Argument(..., help="The references to format."),
-    rough_draft_title: str = typer.Argument(..., help="The rough draft title of the video."),
-    seo_keywords: str = typer.Argument(..., help="The SEO keywords for the video.")
+    rough_draft_title: str = typer.Argument(
+        ..., help="The rough draft title of the video."
+    ),
+    seo_keywords: str = typer.Argument(None, help="The SEO keywords for the video."),
 ):
     create_formatted_references(references, rough_draft_title, seo_keywords)
+
 
 if __name__ == "__main__":
     app()

c90750373499eccd5e6e4aa9b1dc15bf74fa137a
qwukker <minor7addfloortom@gmail.com>
2024-02-13 18:08:30 -0600

    Refactored model validation methods to handle JSON data and updated inquirer checkbox prompts.

--- indy_dev_tools/modules/compose_description.py
+++ indy_dev_tools/modules/compose_description.py
@@ -15,49 +15,52 @@ def compose_description():
     config_file: IdtConfig = load_config()
 
     with open(config_file.yt.description_file_path, "r") as file:
-        descriptions = HighQualityDescriptions.model_validate(file.read())
+        descriptions = HighQualityDescriptions.model_validate_json(file.read())
 
     # hashtags
     with open(config_file.yt.hashtags_file_path, "r") as file:
-        hashtags = HashTagItems.model_validate(file.read())
+        hashtags = HashTagItems.model_validate_json(file.read())
 
     # references
     with open(config_file.yt.formatted_references_file_path, "r") as file:
-        references = ReferenceItems.model_validate(file.read())
-
-    # Prepare the options for the inquirer checkbox prompt
-def compose_description():
-
-    config_file: IdtConfig = load_config()
-
-    with open(config_file.yt.description_file_path, "r") as file:
-        descriptions = HighQualityDescriptions.model_validate(file.read())
+        references = file.read()
 
     hook_options = [desc.hook for desc in descriptions.high_quality_descriptions]
-    first_para_options = [desc.first_paragraph for desc in descriptions.high_quality_descriptions]
-    second_para_options = [desc.second_paragraph for desc in descriptions.high_quality_descriptions]
+    first_para_options = [
+        desc.first_paragraph for desc in descriptions.high_quality_descriptions
+    ]
+    second_para_options = [
+        desc.second_paragraph for desc in descriptions.high_quality_descriptions
+    ]
 
     # Create inquirer checkbox prompts for each section of the description
     hook_question = [
-        inquirer.Checkbox('hook', message="Select a hook", choices=hook_options),
+        inquirer.Checkbox("hook", message="Select a hook", choices=hook_options),
     ]
     first_para_question = [
-        inquirer.Checkbox('first_paragraph', message="Select the first paragraph", choices=first_para_options),
+        inquirer.Checkbox(
+            "first_paragraph",
+            message="Select the first paragraph",
+            choices=first_para_options,
+        ),
     ]
     second_para_question = [
-        inquirer.Checkbox('second_paragraph', message="Select the second paragraph", choices=second_para_options),
+        inquirer.Checkbox(
+            "second_paragraph",
+            message="Select the second paragraph",
+            choices=second_para_options,
+        ),
     ]
 
     # Prompt the user to select one hook, one first paragraph, and one second paragraph
-    selected_hook = inquirer.prompt(hook_question)
-    selected_first_para = inquirer.prompt(first_para_question)
-    selected_second_para = inquirer.prompt(second_para_question)
+    selected_hook = inquirer.prompt(hook_question).get("hook", "")
+    selected_first_para = inquirer.prompt(first_para_question).get(
+        "first_paragraph", ""
+    )
+    selected_second_para = inquirer.prompt(second_para_question).get(
+        "second_paragraph", ""
+    )
 
-    # Extract the selected descriptions
-    final_hook = selected_hook['hook'][0] if selected_hook['hook'] else ""
-    final_first_para = selected_first_para['first_paragraph'][0] if selected_first_para['first_paragraph'] else ""
-    final_second_para = selected_second_para['second_paragraph'][0] if selected_second_para['second_paragraph'] else ""
-
-    # TODO: Implement the logic to use the selected description parts
-    # This could involve writing to a file, returning the values, or further processing
-
+    print("selected_hook", selected_hook)
+    print("selected_first_para", selected_first_para)
+    print("selected_second_para", selected_second_para)

817bb9aa5a26038c79a6133212dee4239bfc3e08
qwukker <minor7addfloortom@gmail.com>
2024-02-23 21:48:12 -0600

    Refactored `sps_prompt` to use `sps_get` function instead of `get_prompt_template_by_alias`.

--- indy_dev_tools/modules/sps_prompt.py
+++ indy_dev_tools/modules/sps_prompt.py
@@ -2,7 +2,7 @@ from typing import Optional
 import openai
 from indy_dev_tools.models import IdtSimplePromptSystem, IdtSimplePromptTemplate
 from indy_dev_tools.modules.idt_config import load_config
-from indy_dev_tools.modules.sps_get import get_prompt_template_by_alias
+from indy_dev_tools.modules.sps_get import sps_get
 from indy_dev_tools.modules.llm import prompt as llm_prompt
 
 
@@ -15,13 +15,13 @@ def sps_prompt(alias: str, prompt: str, vars: Optional[str] = None) -> str:
             "OpenAI API key not found in Simple Prompt System configuration."
         )
 
-    template = get_prompt_template_by_alias(config.sps, alias)
+    template = sps_get(config.sps, alias)
     template_content = template.prompt_template
 
     # Set default values for variables
     variables = {var.name: var.default for var in template.variables}
     # Override defaults with provided values
-    if vars: 
+    if vars:
         custom_vars = dict(var.split("=") for var in vars.split(","))
         for key, value in custom_vars.items():
             if key in variables:

8c31f30100cd80e2b27dc868b640c8348ed04ff1
qwukker <minor7addfloortom@gmail.com>
2024-02-23 21:05:30 -0600

    Added sps_list module for Indy dev tools.

---
+++ indy_dev_tools/modules/sps_list.py
@@ -0,0 +1,13 @@
+from indy_dev_tools.models import IdtSimplePromptSystem
+import typer
+
+def list_prompt_templates(sps_config: IdtSimplePromptSystem):
+    for template in sps_config.templates:
+        typer.echo(f"Alias: {template.alias}")
+        typer.echo(f"Name: {template.name}")
+        typer.echo(f"Description: {template.description}")
+        typer.echo(f"Template: {template.prompt_template}")
+        typer.echo("Variables:")
+        for variable in template.variables:
+            typer.echo(f"  {variable.name} (default: {variable.default}) - {variable.description}")
+        typer.echo("---")

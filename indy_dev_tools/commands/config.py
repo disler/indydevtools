18d20676754a3bf8e79bc6827aec62d08adf7f10
qwukker <minor7addfloortom@gmail.com>
2024-02-25 11:16:55 -0600

    Added commands to edit and view the configuration file.

--- indy_dev_tools/commands/config.py
+++ indy_dev_tools/commands/config.py
@@ -4,11 +4,47 @@ from ..models import IdtConfig
 
 app = typer.Typer()
 
+
 @app.command()
-@app.command(name="")
-def main():
+def edit():
     """
-    Display the current configuration.
+    Open the configuration file in your default editor.
+
+    Input:
+        - None
+    Output:
+        - The configuration file is opened in your default editor.
+    """
+    config: IdtConfig = idt_config.load_config()
+    if config.yt and config.yt.config_file_path:
+        config_file = config.yt.config_file_path
+        typer.launch(config_file)
+
+
+@app.command()
+def view():
+    """
+    View the configuration file in the console.
+
+    Input:
+        - None
+    Output:
+        - The configuration file is printed to the console.
     """
     config: IdtConfig = idt_config.load_config()
     typer.echo(config.model_dump_json(indent=2))
+
+
+@app.callback(invoke_without_command=True)
+def main(ctx: typer.Context):
+    """
+    Print the configuration file to the console.
+
+    Input:
+        - None
+    Output:
+        - The configuration file is printed to the console.
+    """
+    if ctx.invoked_subcommand is None:
+        config: IdtConfig = idt_config.load_config()
+        typer.echo(config.model_dump_json(indent=2))

4cac212ae620221d8caa592b40f949271a21a6d6
qwukker <minor7addfloortom@gmail.com>
2024-02-25 11:40:40 -0600

    Refactored the `view` function in `config.py` to improve readability and maintainability.

--- indy_dev_tools/commands/config.py
+++ indy_dev_tools/commands/config.py
@@ -22,7 +22,14 @@ def edit():
 
 
 @app.command()
-def view(open_in_editor: bool = typer.Option(False, "--open-in-editor", "-o", help="Open the configuration file in the default editor")):
+def view(
+    open_in_editor: bool = typer.Option(
+        False,
+        "--open-in-editor",
+        "-o",
+        help="Open the configuration file in the default editor",
+    )
+):
     """
     View the configuration file in the console.
 
@@ -36,21 +43,3 @@ def view(open_in_editor: bool = typer.Option(False, "--open-in-editor", "-o", he
         typer.launch(config.yt.config_file_path)
     else:
         typer.echo(config.model_dump_json(indent=2))
-
-
-@app.callback(invoke_without_command=True)
-def main(ctx: typer.Context, open_in_editor: bool = typer.Option(False, "--open-in-editor", "-o", help="Open the configuration file in the default editor")):
-    """
-    Print the configuration file to the console.
-
-    Input:
-        - None
-    Output:
-        - The configuration file is printed to the console.
-    """
-    if ctx.invoked_subcommand is None:
-        config: IdtConfig = idt_config.load_config()
-        if open_in_editor and config.yt and config.yt.config_file_path:
-            typer.launch(config.yt.config_file_path)
-        else:
-            typer.echo(config.model_dump_json(indent=2))
